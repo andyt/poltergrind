ee86a8b4b98ff8f79c6a8ad3979b1b7707a8684e
Andy Triggs <andy.triggs@gmail.com>
2015-03-16 18:51:09 +0000

    Renamed example file.

---
+++ examples/google/google_homepage_test.rb
@@ -0,0 +1,19 @@
+require 'poltergrind/worker'
+
+class GoogleHomepageTest
+  include Poltergrind::Worker
+
+  def self.perform
+    10.times { self.perform_async }
+  end
+
+  def perform
+    super do
+      time('google.homepage') do
+        visit 'https://google.com/'
+        puts current_url
+        expect(page).to have_css %Q|input[value="I'm Feeling Lucky"]|
+      end
+    end
+  end
+end
